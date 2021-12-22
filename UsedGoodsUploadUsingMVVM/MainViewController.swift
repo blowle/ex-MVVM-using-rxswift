746c6fee1d89686329448386cbcb4060016d3890
yongcheol Lee <yongcheol.dev@gmail.com>
2021-12-23 02:46:35 +0900

    extend Reactive
    setAlert method

--- UsedGoodsUploadUsingMVVM/MainViewController.swift
+++ UsedGoodsUploadUsingMVVM/MainViewController.swift
@@ -8,11 +8,64 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import SnapKit
 
 class MainViewController: UIViewController {
-
+    
+    let disposeBag = DisposeBag()
+    
+    let tableView = UITableView()
+    let submitButton = UIBarButtonItem()
+    
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    func bind(_ viewModel: MainViewModel) {
+         
+    }
+    
+    private func attribute() {
+        title = "중고거래 글쓰기"
+        view.backgroundColor = .white
+        
+        submitButton.title = "제출"
+        submitButton.style = .done
+        
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .singleLine
+        tableView.tableFooterView = UIView()
+    }
+    
+    private func layout() {
+        navigationItem.setRightBarButton(submitButton, animated: true)
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
 
+typealias Alert = (title: String, message: String?)
+extension Reactive where Base: MainViewController {
+    var setAlert: Binder<Alert> {
+        return Binder(base) { base, data in
+            let alertController = UIAlertController(title: data.title, message: data.message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alertController.addAction(action)
+            base.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
